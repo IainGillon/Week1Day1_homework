38058795657d29bd74607c61291ce93cad972015
Iain Gillon <iaingillon@Iains-MacBook-Air.local>
2021-08-30 19:28:47 +0100

    First homework commit, Day 1 Week1

---
+++ precourse_recap.py
@@ -0,0 +1,12 @@
+# Ask user the names of the town from The Simpsons
+
+Simpsons = input("What town do The Simpsons live in?")
+
+if Simpsons == "Springfield":
+    print("WooHoo!")
+elif Simpsons == "New York":
+    print("New York is that way, man!")
+elif Simpsons == "Shelbyville":
+    print("Close, but no cigar")
+else:
+    print("DOH!")
